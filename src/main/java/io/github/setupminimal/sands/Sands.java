fb384b80a79b90a31fa1a991f8dbef04c0068a76
Daroc Alden <setupminimal@gmail.com>
2015-04-13 10:39:36 -0400

    Episode 3

---
+++ src/main/java/io/github/setupminimal/sands/Sands.java
@@ -0,0 +1,32 @@
+package io.github.setupminimal.sands;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(name="Sands", modid="Sands", version="1.7.2-1.0")
+public class Sands {
+	
+	@Mod.Instance("Sands")
+	public static Sands instance;
+	
+	@Mod.EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		// Network handling, configuration file, register blocks and items
+		;
+	}
+	
+	@Mod.EventHandler
+	public void init(FMLInitializationEvent event) {
+		// GUIs, crafting recipes, tile entities, event handlers
+		;
+	}
+	
+	@Mod.EventHandler
+	public void postInit(FMLPostInitializationEvent event) {
+		// Cleanup, wrapup
+		;
+	}
+
+}
